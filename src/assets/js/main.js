08000f56cd2e748674e1630c1d656804f141171c
kayela-c <kayela@icloud.com>
2022-07-16 21:18:20 -0500

    installed mapbox

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,7 +1,12 @@
-var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js")
 
-mapboxgl.accessToken = "YOUR_MAPBOX_ACCESS_TOKEN"
+
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFyZWluZmVybm8iLCJhIjoiY2tnbGFhbGk2MDB4aTJ5bnVhaWhkdWxwbSJ9.H-4AVylWWGLlgtz89ll1Aw';
 var map = new mapboxgl.Map({
-  container: "pk.eyJ1IjoiaGFyZWluZmVybm8iLCJhIjoiY2tnbGFhbGk2MDB4aTJ5bnVhaWhkdWxwbSJ9.H-4AVylWWGLlgtz89ll1Aw",
-  style: "mapbox://styles/mapbox/streets-v11",
-})
+  container: '#map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+map.addControl(new mapboxgl.NavigationControl());
